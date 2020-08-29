a2137514cdca35d2315faedd7e6f48b0d22eacf9
Florin Munteanu <florinmunteanu96@gmail.com>
2020-08-29 10:25:14 +0300

    Add radix sort

--- src/SortingAlgorithms/SortingAlgorithms.spec.ts
+++ src/SortingAlgorithms/SortingAlgorithms.spec.ts
@@ -1,4 +1,4 @@
-import { HeapSort, QuickSort } from "./SortingAlgorithms";
+import { HeapSort, QuickSort, RadixSort } from "./SortingAlgorithms";
 
 describe("Sorting Algorithms tests", () => {
     it.each`
@@ -28,6 +28,21 @@ describe("Sorting Algorithms tests", () => {
         "Quick Sort tests array $unsorted will become $sorted",
         ({ unsorted, sorted }: { unsorted: number[]; sorted: number[] }) => {
             expect(QuickSort(unsorted)).toStrictEqual(sorted);
+        }
+    );
+
+    it.each`
+        unsorted                                      | sorted
+        ${[9, 1, 5, 2]}                               | ${[1, 2, 5, 9]}
+        ${[1, 0, 0, 1]}                               | ${[0, 0, 1, 1]}
+        ${[1, 2, 3, 4]}                               | ${[1, 2, 3, 4]}
+        ${[5, 4, 3, 2, 1]}                            | ${[1, 2, 3, 4, 5]}
+        ${[91, 9091, 2, 5, 4, 111, 2, 3, 4]}          | ${[2, 2, 3, 4, 4, 5, 91, 111, 9091]}
+        ${[923331, 9091, 1234, 0, 553, 33, 41, 4, 4]} | ${[0, 4, 4, 33, 41, 553, 1234, 9091, 923331]}
+    `(
+        "Radix Sort tests array $unsorted will become $sorted",
+        ({ unsorted, sorted }: { unsorted: number[]; sorted: number[] }) => {
+            expect(RadixSort(unsorted, (e) => e)).toStrictEqual(sorted);
         }
     );
 });
--- src/SortingAlgorithms/SortingAlgorithms.ts
+++ src/SortingAlgorithms/SortingAlgorithms.ts
@@ -33,13 +33,18 @@ export function QuickSort<T>(
 ): T[] {
     const result = [...elements];
     const sort = (arr: T[], start: number, end: number) => {
-        if(start>=end) return;
+        if (start >= end) return;
 
         const pivot = arr[end];
-        let i = start-1;
+        let i = start - 1;
 
-        for(let j=start;j<=end;j++) {
-            if(compareFunction(getComparisonValue(pivot), getComparisonValue(arr[j])) <= 0) {
+        for (let j = start; j <= end; j++) {
+            if (
+                compareFunction(
+                    getComparisonValue(pivot),
+                    getComparisonValue(arr[j])
+                ) <= 0
+            ) {
                 i++;
                 const tmp = arr[i];
                 arr[i] = arr[j];
@@ -47,11 +52,63 @@ export function QuickSort<T>(
             }
         }
 
-        sort(arr, start, i-1);
-        sort(arr, i+1, end);
-    }
+        sort(arr, start, i - 1);
+        sort(arr, i + 1, end);
+    };
 
-    sort(result, 0, elements.length-1);
+    sort(result, 0, elements.length - 1);
+
+    return result;
+}
+
+/**
+ *
+ * @param elements Array of T
+ * @param getComparisonValue Function that returns a !positive! number used in sorting based on an item from elements
+ */
+export function RadixSort<T>(
+    elements: T[],
+    getComparisonValue: GetComparisonValueFunc<T, number>
+): T[] {
+    let result: T[] = [...elements];
+    const getDigitAt = (n: number, pos: number): number => {
+        if (pos > 0 && n == 0) return undefined;
+        if (pos == 0) return n % 10;
+        return getDigitAt(Math.floor(n / 10), pos - 1);
+    };
+
+    let hasDigits = true;
+    let digitPos = 0;
+
+    while (hasDigits) {
+        hasDigits = false;
+        const arr = [];
+
+        for (const e of result) {
+            let digit = getDigitAt(getComparisonValue(e), digitPos);
+
+            if (digit != undefined) {
+                hasDigits = true;
+            } else {
+                digit = 0;
+            }
+
+            if (arr[digit] == undefined) {
+                arr[digit] = [];
+            }
+
+            arr[digit].push(e);
+        }
+
+        result = [];
+        arr.forEach((e) => {
+            e.forEach((e) => {
+                result.push(e);
+            });
+        });
+
+        digitPos++;
+    }
 
     return result;
 }
